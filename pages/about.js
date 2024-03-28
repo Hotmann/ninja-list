7d6d8a97508a823727ff09b1fb2220e86b414130
Ogunbowale Uthman <hotmann442@gmail.com>
2024-03-28 00:30:29 -0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          loremEt nisi fugiat Lorem sit. Ipsum in Lorem sit cillum laborum.
+          Fugiat magna nostrud cupidatat adipisicing voluptate amet sint tempor
+          do incididunt in officia veniam. Tempor veniam voluptate eu aliqua in
+          ipsum cillum mollit occaecat sit est. Tempor dolore laborum cillum
+          ipsum et
+        </p>
+        <p>
+          Ullamco non tempor labore non anim eiusmod veniam dolor. Amet laborum
+          incididunt quis culpa sint duis pariatur quis. Sunt non officia labore
+          commodo amet eiusmod voluptate esse ipsum irure ea exercitation aliqua
+          nulla. Nostrud ea esse anim quis exercitation est proident veniam
+          nostrud eiusmod. Velit pariatur velit reprehenderit velit minim id
+          officia irure esse fugiat exercitation enim deserunt.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
